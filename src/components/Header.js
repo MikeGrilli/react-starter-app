4defa02dc70662222f1d1b97f907ccfe95487af6
MikeGrilli <your-personal-email@gmail.com>
2025-07-30 16:28:02 -0700

    Title change

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,9 @@ function Header() {
   return (
     <header className="header">
       <nav>
-        <h2>I made a counter app</h2>
+        <h2>
+          Hey I made a Counter app, hooked it up to git up and deployed it
+        </h2>
       </nav>
     </header>
   );
